0c8a112801bd99d437ce721ef9ef6a95cca99840
yusuke0701 <yuusukerego@yahoo.co.jp>
2020-05-11 17:45:34 +0900

    grpc-serverの作成

---
+++ server/go.mod
@@ -0,0 +1,10 @@
+module server
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.4.1
+	google.golang.org/genproto v0.0.0-20200507105951-43844f6eee31
+	google.golang.org/grpc v1.29.1
+	google.golang.org/protobuf v1.22.0
+)
--- server/scripts/generate.sh
+++ server/scripts/generate.sh
@@ -4,8 +4,10 @@ cd `dirname $0`
 
 cd ../../
 
+# TODO: googleapisのバージョン指定はダサい
+
 protoc -Iproto \
   -I$GOPATH/src \
-  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  -I$GOPATH/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.14.5/third_party/googleapis \
   --go_out=plugins=grpc:./server \
   proto/*.proto
