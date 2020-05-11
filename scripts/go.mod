4232684a2a3d08bc56dc2d64033bf6633ba5b1ba
yusuke0701 <yuusukerego@yahoo.co.jp>
2020-05-11 17:40:36 +0900

    grpc-gatewayで使うツールを導入

---
+++ scripts/go.mod
@@ -0,0 +1,8 @@
+module tools
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.4.1
+	github.com/grpc-ecosystem/grpc-gateway v1.14.5
+)
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,7 @@
 
 cd `dirname $0`
 
+go mod download
 go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
 go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
 go install github.com/golang/protobuf/protoc-gen-go
