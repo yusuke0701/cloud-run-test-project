b4b9ff865a5d99ba1a697bad9f3948169302f1b9
yusuke-home <yuusukerego@yahoo.co.jp>
2020-05-13 15:28:55 +0900

    gRPCサーバーを叩くための認証情報を入れる

--- gateway/go.mod
+++ gateway/go.mod
@@ -3,11 +3,18 @@ module gateway
 go 1.14
 
 require (
+	github.com/coreos/go-oidc v2.2.1+incompatible // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/golang/protobuf v1.4.1
+	github.com/google/go-tpm v0.2.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway v1.14.5
-	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f
-	google.golang.org/genproto v0.0.0-20190927181202-20e1ac93f88c
+	github.com/hashicorp/vault/api v1.0.4 // indirect
+	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
+	github.com/salrashid123/oauth2 v0.0.0-20200503195646-e37a24dfdeb3
+	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	google.golang.org/api v0.24.0 // indirect
+	google.golang.org/genproto v0.0.0-20200331122359-1ee6d9798940
 	google.golang.org/grpc v1.29.1
 	google.golang.org/protobuf v1.22.0
 )
--- gateway/main.go
+++ gateway/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"crypto/x509"
 	"flag"
 	"fmt"
 	"gateway/gen/api"
@@ -9,36 +11,86 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
-	"golang.org/x/net/context"
+	sal "github.com/salrashid123/oauth2/google"
+	"golang.org/x/oauth2/google"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
-const endpoint = "https://server-5dnafyaz7q-ue.a.run.app"
+const (
+	httpPort = "8081"
+)
+
+const (
+	address  = "server-5dnafyaz7q-ue.a.run.app"
+	audience = "https://server-5dnafyaz7q-ue.a.run.app"
+)
 
 func main() {
 	flag.Parse()
 	defer glog.Flush()
 
-	if err := run(); err != nil {
+	if err := runGW(); err != nil {
 		glog.Fatal(err)
 	}
 }
 
-func run() error {
+func runGW() error {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	mux := runtime.NewServeMux()
-	opts := []grpc.DialOption{grpc.WithInsecure()}
-	err := api.RegisterPancakeBakerServiceHandlerFromEndpoint(ctx, mux, endpoint, opts)
+	pool, _ := x509.SystemCertPool()
+	creds := credentials.NewClientTLSFromCert(pool, "")
+	perRPC, err := getRPCCreds(ctx)
 	if err != nil {
 		return err
 	}
 
+	// Register gRPC server endpoint
+	// Note: Make sure the gRPC server is running properly and accessible
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(creds),
+		grpc.WithPerRPCCredentials(perRPC),
+		grpc.WithBlock(),
+	}
+
+	grpcServerEndpoint := flag.String("grpc-server-endpoint", fmt.Sprintf("%s:%s", address, "443"), "gRPC server endpoint")
+
+	if err := api.RegisterPancakeBakerServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts); err != nil {
+		return err
+	}
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "50050"
+		port = httpPort
 	}
+	// Start HTTP server (and proxy calls to gRPC server endpoint)
 	return http.ListenAndServe(fmt.Sprintf(":%s", port), mux)
+}
+
+func getRPCCreds(ctx context.Context) (credentials.PerRPCCredentials, error) {
+	scopes := "https://www.googleapis.com/auth/userinfo.email"
+
+	creds, err := google.FindDefaultCredentials(ctx, scopes)
+	if err != nil {
+		return nil, err
+	}
+
+	idTokenSource, err := sal.IdTokenSource(
+		&sal.IdTokenConfig{
+			Credentials: creds,
+			Audiences:   []string{audience},
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	rpcCreds, err := sal.NewIDTokenRPCCredential(ctx, idTokenSource)
+	if err != nil {
+		return nil, err
+	}
+
+	return rpcCreds, nil
 }
