f9c5aa66daa241e647d3bf5ec833f7d333e51901
yusuke-home <yuusukerego@yahoo.co.jp>
2020-07-28 23:57:15 +0900

    use submodule

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -6,12 +6,12 @@ cd ../
 
 protoc -Iproto \
     -I$GOPATH/src \
-    -I$GOPATH/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.14.6/third_party/googleapis \
+    -Igrpc-gateway/third_party/googleapis \
     --go_out=plugins=grpc:./server \
     proto/*.proto
 
 protoc -Iproto \
     -I$GOPATH/src \
-    -I$GOPATH/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.14.6/third_party/googleapis \
+    -Igrpc-gateway/third_party/googleapis \
     --grpc-gateway_out=logtostderr=true:./gateway \
     proto/*.proto
